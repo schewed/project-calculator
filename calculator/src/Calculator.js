ed124605a9b7f47997dd250de03748dbd4d3ecdc
Daniel Schewe <danielschewe@Daniels-MacBook-Pro.local>
2019-09-30 12:36:21 -0600

    added calculator and display components

---
+++ calculator/src/Calculator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Calculator(props) {
+    return(
+    <div className="calculator">
+        This is the calculator that will house the display and buttons
+    </div>
+    )};
+
+export default Calculator;
